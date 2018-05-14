28874c32e985b72fb926f2ae2d0da88695897b10
Rehna Raju <rehnaraju111@gmail.com>
2018-05-14 09:46:45 +0200

    Commit sert

---
+++ ScoutVerwaltung/src/de/gfn/oca/scoutcamp/entity/Scout.java
@@ -0,0 +1,81 @@
+package de.gfn.oca.scoutcamp.entity;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author tlubowiecki
+ */
+public class Scout {
+    
+    private int id;
+    private String firstname;
+    private String lastname;
+    private LocalDate birthdate;
+    private boolean active;
+
+    public Scout() {
+    }
+    
+    public Scout(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Name:")
+                .append(firstname)
+                .append(" ")
+                .append(lastname)
+                .append("\n")
+                .append("Geburtrsdatum: ")
+                .append(birthdate)
+                .append("\n")
+                .append("Aktiv: ")
+                .append(active);
+        
+        return sb.toString();
+    }
+}
